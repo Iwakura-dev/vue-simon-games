2f0468dd7766e2fa7fb1b219beda0fdfac63a4f4
Iwakura-dev <escayhokage@gmail.com>
2024-03-15 18:46:41 -0700

    fix: 🐛 path

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
- git push -f git@github.com:iwakura-dev/vue-test-task-simon-games.git main:gh-pages
+ git push -f git@github.com:iwakura-dev/vue-simon-games.git main:gh-pages
 
 cd -
