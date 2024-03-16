45e83a728d611424063bfe4ca47ecf4b3b4514fc
Iwakura-dev <escayhokage@gmail.com>
2024-03-15 18:45:18 -0700

    fix: 🐛 path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   publicPath:
-    process.env.NODE_ENV === "production" ? "/vue-test-task-simon-games/" : "/",
+    process.env.NODE_ENV === "production" ? "/vue-simon-games/" : "/",
   transpileDependencies: true,
 });
